8d57cace2305247e884bc82943ab036b454c9a07
Arvind Vyas <arvindvyas07@gmail.com>
2015-12-15 00:02:25 +0530

    Fixed issue of locals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
-  I18n.locale = lang
 
   if defined?(Refinery::User)
     Refinery::User.all.each do |user|
@@ -10,10 +8,7 @@
     end
   end
 
-end
 
-(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
-  I18n.locale = lang
 
   if defined?(Refinery::User)
     Refinery::User.all.each do |user|
@@ -24,4 +19,3 @@ end
     end
   end
 
-end
